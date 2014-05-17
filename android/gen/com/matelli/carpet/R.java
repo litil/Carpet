9f29d1b2605f95e6424a1e06038c89e3e4e44e4e
erodier <erodier@gmail.com>
2014-05-17 11:15:29 +0200

    Test récupération Geoloc

--- android/gen/com/matelli/carpet/R.java
+++ android/gen/com/matelli/carpet/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int button_geo=0x7f080002;
         public static final int button_goto_conducteur=0x7f080001;
         public static final int button_goto_enfant=0x7f080000;
     }
@@ -43,6 +44,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int goto_conducteur=0x7f050007;
         public static final int goto_enfant=0x7f050006;
+        public static final int goto_geo=0x7f050008;
         public static final int hello_world=0x7f050002;
         public static final int title_activity_conducteur=0x7f050003;
         public static final int title_activity_enfant=0x7f050004;
@@ -51,10 +53,10 @@ public final class R {
         public static final int welcome_carpet=0x7f050005;
         /**  CONDUCTEUR 
          */
-        public static final int welcome_conducteur=0x7f050009;
+        public static final int welcome_conducteur=0x7f05000a;
         /**  ENFANT 
          */
-        public static final int welcome_enfant=0x7f050008;
+        public static final int welcome_enfant=0x7f050009;
     }
     public static final class style {
         /** 
