21d6b48bbe5ca759e5b1eb4948b3717f214c3d60
Florian BUREL <fl0@mac.com>
2014-05-17 14:53:59 +0200

    Ajout du sous projet anddev

---
+++ android/src/com/matelli/carpet/game/Scene/SplashScene.java
@@ -0,0 +1,50 @@
+package com.matelli.carpet.game.Scene;
+
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.util.GLState;
+
+/**
+ * Created by fl0 on 17/05/2014.
+ */
+public class SplashScene extends BaseScene {
+
+    private Sprite splash;
+
+    @Override
+    public void createScene() {
+        splash = new Sprite(0, 0, resourcesManager.splash_region, vbom)
+        {
+            @Override
+            protected void preDraw(GLState pGLState, Camera pCamera)
+            {
+                super.preDraw(pGLState, pCamera);
+                pGLState.enableDither();
+            }
+        };
+
+        splash.setScale(1.5f);
+        splash.setPosition(400, 240);
+        attachChild(splash);
+    }
+
+    @Override
+    public void onBackKeyPressed() {
+
+    }
+
+    @Override
+    public SceneManager.SceneType getSceneType() {
+        return SceneManager.SceneType.SCENE_SPLASH;
+    }
+
+    public void disposeScene()
+    {
+        splash.detachSelf();
+        splash.dispose();
+        this.detachSelf();
+        this.dispose();
+    }
+}
