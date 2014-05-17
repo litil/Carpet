50b98906f52440cedffafc7a09ddfa72a70f81a4
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2014-05-17 13:20:05 +0200

    Added the signing with Facebook button

--- android/src/com/matelli/carpet/activities/WelcomeActivity.java
+++ android/src/com/matelli/carpet/activities/WelcomeActivity.java
@@ -1,116 +1,85 @@
 package com.matelli.carpet.activities;
 
-import com.matelli.carpet.R;
-import com.matelli.carpet.R.id;
-import com.matelli.carpet.R.layout;
-import com.matelli.carpet.R.menu;
-
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+
+import com.matelli.carpet.R;
 
 public class WelcomeActivity extends RoboActivity {
 	private static final String TAG = "WelcomeActivity";
-	
-	@InjectView(R.id.button_goto_enfant)		Button gotoEnfant; 
-	@InjectView(R.id.button_goto_conducteur)	Button gotoConducteur; 
-	@InjectView(R.id.button_geo)				Button testGeo; 
-	
+
+	@InjectView(R.id.button_goto_enfant)		RelativeLayout gotoEnfant; 
+
 	private Activity currentActivity;
-	
+
 	private double longitude = 0f;
 	private double latitude = 0f;
 	private LocationManager lm;
-	
-//	private final LocationListener locationListener = new LocationListener() {
-//	    public void onLocationChanged(Location location) {
-//	        longitude = location.getLongitude();
-//	        latitude = location.getLatitude();
-//	        Log.v("TEST", "Longitude : " + longitude + " - Latitude : " + latitude);
-//	    }
-//
-//		@Override
-//		public void onProviderDisabled(String provider) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//
-//		@Override
-//		public void onProviderEnabled(String provider) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//
-//		@Override
-//		public void onStatusChanged(String provider, int status, Bundle extras) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//	};
 
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-        currentActivity = this;
-        
-        // got to EnfantActivity
-        gotoEnfant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            	Intent intent = new Intent(currentActivity, EnfantActivity.class);
-            	currentActivity.startActivity(intent);
-            }
-        });
-        
-     // got to ConducteurActivity
-        gotoConducteur.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            	Intent intent = new Intent(currentActivity, ConducteurActivity.class);
-            	currentActivity.startActivity(intent);
-            }
-        });
-        
-        testGeo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            	 Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                 double longitud = location.getLongitude();
-                 double latitud = location.getLatitude();
-                 Toast.makeText(currentActivity, ("Longitude : " + longitud + " - Latitude : " + latitud), Toast.LENGTH_LONG).show();
-            }
-        });
-        
-        
-       
-        
-        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
+	//	private final LocationListener locationListener = new LocationListener() {
+	//	    public void onLocationChanged(Location location) {
+	//	        longitude = location.getLongitude();
+	//	        latitude = location.getLatitude();
+	//	        Log.v("TEST", "Longitude : " + longitude + " - Latitude : " + latitude);
+	//	    }
+	//
+	//		@Override
+	//		public void onProviderDisabled(String provider) {
+	//			// TODO Auto-generated method stub
+	//			
+	//		}
+	//
+	//		@Override
+	//		public void onProviderEnabled(String provider) {
+	//			// TODO Auto-generated method stub
+	//			
+	//		}
+	//
+	//		@Override
+	//		public void onStatusChanged(String provider, int status, Bundle extras) {
+	//			// TODO Auto-generated method stub
+	//			
+	//		}
+	//	};
 
-//        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
-    }
 
-    
-    
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+		currentActivity = this;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.welcome, menu);
-        return true;
-    }
-    
+		// got to EnfantActivity
+		gotoEnfant.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(currentActivity, EnfantActivity.class);
+				currentActivity.startActivity(intent);
+			}
+		});
+
+		lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
+
+		//        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+	}
+
+
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.welcome, menu);
+		return true;
+	}
+
 }
